6965cf98eac42476ab8dfda3641916f94c00ae26
ivonaH <ivonaheldrih@gmail.com>
2020-02-19 14:50:47 +0100

    Dodate toString, hashCode i equals metode.

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -40,6 +40,37 @@ public class Knjiga {
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
 	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", ISBN=" + ISBN + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + ", getNaslov()=" + getNaslov() + ", getISBN()=" + getISBN()
+				+ ", getAutori()=" + getAutori() + ", getIzdavac()=" + getIzdavac() + ", getIzdanje()=" + getIzdanje()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ISBN == null) ? 0 : ISBN.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (ISBN == null) {
+			if (other.ISBN != null)
+				return false;
+		} else if (!ISBN.equals(other.ISBN))
+			return false;
+		return true;
+	}
 	
 	
 	
